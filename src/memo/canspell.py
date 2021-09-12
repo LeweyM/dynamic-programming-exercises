6f68b95aa2d16d2ab2168ccec02249ce37dd91e7
Lewis Metcalf <leweymetcalf@hotmail.co.uk>
2021-09-12 13:17:25 +0100

    can spell memoization complexity analysis

--- src/memo/canspell.py
+++ src/memo/canspell.py
@@ -16,3 +16,22 @@ def can_spell(target, words, memo=None):
                 return True
 
     return False
+
+# Complexity
+#
+# Time:
+#
+# m : len(target)
+# n : number of words in word list
+#
+# The algorithm enumerates the branches of the spelling tree, which would have depth m and branching
+# factor n (n^m). However, memoization ensures that substrings of the target are processed only once. At each
+# processing, however, we must still process each word in word list
+# Without memoization: O(n^m)
+# With memoization: O(m*n)
+#
+# Memory:
+#
+# Call stack grows with depth of tree as factor of target length (m). Memoization dict also grows as factor
+# of target length.
+# O(m)
